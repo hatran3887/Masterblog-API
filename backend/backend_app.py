d7c8659623f1ab90d62435c56011b8d86adaed80
Ha Tran <hatran3887@googlemail.com>
2026-06-25 15:02:05 +0200

    Add update and search function

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -10,40 +10,68 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET', 'POST'])
+@app.route('/api/posts', methods=['GET'])
 def get_posts():
     """
-    Posts route for both GET and POST, POST for create a new post and GET to show all posts
+    Posts route to show all posts
     """
-    if request.method == 'POST':
-        data = request.get_json()
-        required_fields = ['title', 'content']
-        missing = [field for field in required_fields if field not in data]
-
-        if missing:
-            return jsonify({'Error': f'Missing required field: {missing}'}), 400
-
-        current_max_id = max([post['id'] for post in POSTS])
-        new_post = {'id': current_max_id + 1, 'title': data['title'], 'content': data['content']}
-        POSTS.append(new_post)
-        return jsonify(new_post, 201)
-
     return jsonify(POSTS)
 
 
-@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
-def delete_post(post_id):
+@app.route('/api/posts', methods=['POST'])
+def create_posts():
     """
-    Delete a post with given id
+    Posts route for create a new post
+    """
+    data = request.get_json()
+    required_fields = ['title', 'content']
+    missing = [field for field in required_fields if field not in data]
+
+    if missing:
+        return jsonify({'Error': f'Missing required field: {missing}'}), 400
+
+    current_max_id = max([post['id'] for post in POSTS])
+    new_post = {'id': current_max_id + 1, 'title': data['title'], 'content': data['content']}
+    POSTS.append(new_post)
+    return jsonify(new_post, 201)
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE', 'PUT'])
+def handle_post(post_id):
+    """
+    Delete or update a post with given id
     """
     post_id = int(post_id)
     post = next((post for post in POSTS if post['id'] == post_id), None)
 
-    if post:
+    if not post:
+        return jsonify({'message': f'Post with the id {post_id} was not found.'}), 404
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        post['title'] = data.get('title', post['title'])
+        post['content'] = data.get('content', post['content'])
+        return jsonify(post)
+    else:
         POSTS.remove(post)
         return jsonify({'message': f'Post with the id {post_id} has been deleted successfully.'}), 200
-    return jsonify({'message': f'Post with the id {post_id} was not found.'}), 404
 
+
+@app.route('/api/posts/search', methods=['GET'])
+def search_posts():
+    """
+    Search route to search all posts
+    """
+    searched_title = request.args.get('title')
+    searched_content = request.args.get('content')
+
+    posts = []
+    if searched_title:
+        posts = [post for post in POSTS if searched_title.lower() in post['title'].lower()]
+    if searched_content:
+        posts = [post for post in POSTS if searched_content.lower() in post['content'].lower()]
+
+    return jsonify(posts), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
